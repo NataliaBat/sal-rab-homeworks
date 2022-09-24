b3a1290fe62b71dd6451215709c568e3eed826d8
Natalia B <batulinann@gmail.ru>
2022-09-24 09:25:30 +0300

    correction

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,20 +1,17 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-    let data;
-    data = JSON.parse(json);
+    let  = JSON.parse(json);
     
-    let products;
-    products = data.products;
+    let products = data.products;
    
     return products;
 }
 
 function renderProductsCards(json) {
     clearProducts();
-    let products;
-    products = parseProducts(json);
-    lenght = products.lenght;
+    let products = parseProducts(json);
+    length = products.length;
 
     for (let i = 1; i < length; i += 1) {
         addProduct(products[i]);
